fff11dd2d54d1391a09a0a6961b72629b7500105
sankardevts <sankar@orbiosolutions.com>
2015-01-27 19:21:34 +0530

    comment some code

--- Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Scripts/main.js
+++ Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Scripts/main.js
@@ -1,7 +1,15 @@
 /*price range*/
 
- $('#sl2').slider();
-
+//$("#sl2").slider({
+//    range: true,
+//    min: 0,
+//    max: 500,
+//    values: [35, 200],
+//    slide: function (event, ui) {
+//        $("#slidevalue")
+//           .val("$" + ui.values[0] + " - $" + ui.values[1]);
+//    }
+//});
 	var RGBChange = function() {
 	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
 	};	
