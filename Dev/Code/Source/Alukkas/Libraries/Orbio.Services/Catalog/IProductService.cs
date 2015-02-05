fd5a013d5e5f94d5f0d64166660a834018825183
sankardevts <sankar@orbiosolutions.com>
2015-02-05 17:35:44 +0530

    Add product services

---
+++ Dev/Code/Source/Alukkas/Libraries/Orbio.Services/Catalog/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orbio.Core.Domain.Catalog;
+
+namespace Orbio.Services.Catalog
+{
+    public interface IProductService
+    {
+
+        /// <summary>
+        /// gets product details by slug
+        /// </summary>
+        /// <param name="slug">the slug value</param>
+        /// <param name="entityName">the entity name</param>
+        /// <returns>instance of Product details</returns>
+        Product GetProductsDetailsBySlug(string slug);
+
+    }
+}
---
+++ Dev/Code/Source/Alukkas/Libraries/Orbio.Services/Catalog/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using Orbio.Core.Domain.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Domain;
+using Nop.Data;
+using System.Linq;
+using Orbio.Services.Utility;
+using System.Data.SqlClient;
+
+namespace Orbio.Services.Catalog
+{
+    class ProductService:IProductService
+    {
+
+         private readonly IDbContext context;
+        private const string entityName = "Product";
+
+        /// <summary>
+        /// instantiates Category service type
+        /// </summary>
+        /// <param name="context">db context</param>
+        public ProductService(IDbContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// gets all productsdetails by slug
+        /// </summary>
+        /// <param name="slug">the slug value</param>
+        /// <param name="entityName">the entity name</param>
+        /// <returns>list of products</returns>
+
+        public Product GetProductsDetailsBySlug(string slug)
+        {
+            var sqlParamList = new List<SqlParameter>();
+            sqlParamList.Add(new SqlParameter() { ParameterName = "@slug", Value = slug, DbType = System.Data.DbType.String });
+            sqlParamList.Add(new SqlParameter { ParameterName = "@entityName", Value = entityName, DbType = System.Data.DbType.String });
+
+            var result = context.ExecuteFunction<XmlResultSet>("usp_Catalog_GetProductsDetailBySlug",
+                sqlParamList.ToArray()
+                ).FirstOrDefault();
+            if (result != null)
+            {
+                var productdetails = Serializer.GenericDeSerializer<Product>(result.XmlResult);
+                return productdetails;
+            }
+
+            return new Product();
+        }
+    }
+}
