680959acc18e5a860ab03eba9a86cd97c1ca823e
Orbioadmin <rakesh@orbiosolutions.com>
2015-02-23 21:11:14 +0530

    product detail

--- Dev/Code/Source/Alukkas/Libraries/Orbio.Core/Domain/Catalog/ProductDetail.cs
+++ Dev/Code/Source/Alukkas/Libraries/Orbio.Core/Domain/Catalog/ProductDetail.cs
@@ -6,6 +6,62 @@ namespace Orbio.Core.Domain.Catalog
     [DataContract]
     public class ProductDetail : Product
     {
+        /// <summary>
+        /// Gets or sets the stock quantity
+        /// </summary>
+        public int StockQuantity { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is ship enabled
+        /// </summary>
+        public bool IsShipEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is free shipping
+        /// </summary>
+        public bool IsFreeShipping { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether to display stock availability
+        /// </summary>
+        public bool DisplayStockAvailability { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to display stock quantity
+        /// </summary>
+        public bool DisplayStockQuantity { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating how to manage inventory
+        /// </summary>
+        public int ManageInventoryMethodId { get; set; }
+        /// <summary>
+        /// Gets or sets the value indicating how to manage inventory
+        /// </summary>
+        public ManageInventoryMethod ManageInventoryMethod
+        {
+            get
+            {
+                return (ManageInventoryMethod)this.ManageInventoryMethodId;
+            }
+            set
+            {
+                this.ManageInventoryMethodId = (int)value;
+            }
+        }
+        /// <summary>
+        /// Gets or sets a value of used category template viewpath
+        /// </summary>
+        [DataMember]
+        public string ProductViewPath { get; set; }
+
+        /// <summary>
+        /// gets or sets categories for breadcrumbs
+        /// </summary>
+        [DataMember]
+        public List<Category> BreadCrumbs { get; set; }
+
+        /// <summary>
+        /// gets or sets categories for DeliveredIn
+        /// </summary>
+        [DataMember]
         public string DeliveredIn { get; set; }
         /// <summary>
         /// gets all Product Specification Attributes
--- Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Models/Catalog/ProductDetailModel.cs
+++ Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Models/Catalog/ProductDetailModel.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.IO;
 
 namespace Orbio.Web.UI.Models.Catalog
 {
@@ -14,21 +16,28 @@ namespace Orbio.Web.UI.Models.Catalog
             this.ProductVariantAttributes = new List<ProductVariantAttributeModel>();
             this.SpecificationAttributes = new List<SpecificationAttribute>();
         }
-        public ProductDetailModel(ProductDetail ProductDetail)
-            : this()
+        public ProductDetailModel(ProductDetail productDetail) : base(productDetail)
+            
         {
-            this.Id = ProductDetail.Id;
-            this.Name = ProductDetail.Name;
-            this.ShortDescription = ProductDetail.ShortDescription;
-            this.FullDescription = ProductDetail.FullDescription;
-            this.SeName = ProductDetail.SeName;
-            this.ViewPath = ProductDetail.ViewPath;
-            this.ImageRelativeUrl = ProductDetail.ImageRelativeUrl;
-            this.CurrencyCode = ProductDetail.CurrencyCode;
-            this.ProductPrice.Price = ProductDetail.Price.ToString("0.00");
-            if (ProductDetail.SpecificationFilters != null && ProductDetail.SpecificationFilters.Count > 0)
+            this.Id = productDetail.Id;
+            this.Name = productDetail.Name;
+            this.ShortDescription = productDetail.ShortDescription;
+            this.FullDescription = productDetail.FullDescription;
+            this.SeName = productDetail.SeName;
+            this.ViewPath = productDetail.ViewPath;
+            this.ImageRelativeUrl = productDetail.ImageRelativeUrl;
+            this.CurrencyCode = productDetail.CurrencyCode;
+            this.ProductPrice.Price = productDetail.Price.ToString("0.00");
+
+             if (productDetail.BreadCrumbs != null && productDetail.BreadCrumbs.Count > 0)
+            {               
+                this.BreadCrumbs = (from c in productDetail.BreadCrumbs
+                                    select new CategoryModel { Name = c.Name, SeName = c.SeName }).ToList();
+            }
+
+            if (productDetail.SpecificationFilters != null && productDetail.SpecificationFilters.Count > 0)
             {
-                var specFilterByspecAttribute = from sa in ProductDetail.SpecificationFilters
+                var specFilterByspecAttribute = from sa in productDetail.SpecificationFilters
                                                 group sa by sa.SpecificationAttributeName;
                 //var currentUrl = ControllerContext.RequestContext.HttpContext.Request.Url.AbsoluteUri;
 
@@ -49,9 +58,56 @@ namespace Orbio.Web.UI.Models.Catalog
                                                                              }))
                              }).ToList();
             }
+
+            if (productDetail.ProductPictures != null && productDetail.ProductPictures.Count > 0)
+            {
+                var baseUrl = ConfigurationManager.AppSettings["ImageServerBaseUrl"];
+                this.ProductPictures = (from pp in productDetail.ProductPictures
+                                        select new PictureModel
+                                        {
+                                            ImageUrl = pp.RelativeUrl != null ? baseUrl + GetThumbImageFileName(pp.RelativeUrl) : string.Empty,
+                                            FullSizeImageUrl = pp.RelativeUrl != null ? baseUrl + pp.RelativeUrl : string.Empty,
+                                            AlternateText = productDetail.Name,
+                                            Title = productDetail.ShortDescription
+                                        }).ToList();
+
+                this.DefaultPicture = this.ProductPictures.First();
+            }
+
+            this.StockAvailability = productDetail.FormatStockMessage();
+            this.IsFreeShipping = productDetail.IsFreeShipping;
+            this.IsShipEnabled = productDetail.IsShipEnabled;
+            this.DeliveredIn = productDetail.DeliveredIn;
         }
         public List<ProductVariantAttributeModel> ProductVariantAttributes { get; private set; }
 
         public List<SpecificationAttribute> SpecificationAttributes { get; private set; }
+        public string DeliveredIn { get; set; }
+
+        public IList<CategoryModel> BreadCrumbs { get; set; }
+
+        public IList<PictureModel> ProductPictures { get; set; }
+
+        public PictureModel DefaultPicture { get; set; }
+
+        public string StockAvailability { get; set; }
+
+        public bool IsShipEnabled { get; set; }
+
+        public bool IsFreeShipping { get; set; }
+
+        private static string GetThumbImageFileName(string imageUrl)
+        {
+            var fileName = imageUrl;
+            if (imageUrl.IndexOf('?') > 0)
+            {
+                fileName = imageUrl.Substring(0, imageUrl.IndexOf('?'));
+            }
+
+            fileName = fileName.Substring(fileName.LastIndexOf('/') + 1, fileName.Length - fileName.LastIndexOf('/') - 1);
+            fileName = Path.GetFileNameWithoutExtension(fileName);
+
+            return imageUrl.Replace(fileName, fileName + "_tb");
+        }
     }
 }
