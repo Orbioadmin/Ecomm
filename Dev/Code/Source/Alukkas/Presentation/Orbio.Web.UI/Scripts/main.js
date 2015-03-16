f72fa2af9f0fdd94e724c9c0c9e30d9e08d78771
sankardevts <sankar@orbiosolutions.com>
2015-03-16 19:14:17 +0530

    cart success

--- Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Scripts/main.js
+++ Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Scripts/main.js
@@ -50,3 +50,24 @@ var ShowError = function (msg) {
 
     toastr.error(msg);
 };
+var ShowSuccessMsg = function (msg) {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-full-width",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr.success(msg);
+};
