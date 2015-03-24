e8de23e57f962aac7f9d92b852f7245bdf7cae81
sankardevts <sankar@orbiosolutions.com>
2015-03-25 01:43:17 +0530

    bug fixed in category product page

--- Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Controllers/CatalogController.cs
+++ Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Controllers/CatalogController.cs
@@ -123,7 +123,7 @@ namespace Orbio.Web.UI.Controllers
             {
 
                 model = PrepareCategoryProductModelBySearch(seName, spec, minPrice, maxPrice, keyword);
-                ViewBag.searchkeyword = " BY KEYWORD " + keyword;
+                ViewBag.searchkeyword = " BY KEYWORD ''" + keyword+"''";
                 //ViewBag.MetaDescription = model.MetaDescription;
                 //ViewBag.MetaKeywords = model.MetaKeywords;
             }
@@ -141,7 +141,7 @@ namespace Orbio.Web.UI.Controllers
         {
             var model = PrepareCategoryProductModel(seName, spec, keyword);
             if (!string.IsNullOrEmpty(keyword))
-            { ViewBag.searchkeyword = " ITEMS FOUND BY KEYWORD " + keyword; }
+            { ViewBag.searchkeyword = " ITEMS FOUND BY KEYWORD ''" + keyword + "''"; }
             var queryString = new NameValueCollection(ControllerContext.HttpContext.Request.QueryString);
             webHelper.RemoveQueryFromPath(ControllerContext.HttpContext, new List<string> { { "spec" } });
             ViewBag.MetaDescription = model.MetaDescription;
