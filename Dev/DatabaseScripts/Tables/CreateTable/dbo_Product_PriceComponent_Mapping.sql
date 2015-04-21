5f04506150446f077598076f66e3deb8d20928fa
sankardevts <sankar@orbiosolutions.com>
2015-04-21 12:21:52 +0530

    Created new tables and fixed bugs in responsive

---
+++ Dev/DatabaseScripts/Tables/CreateTable/dbo_Product_PriceComponent_Mapping.sql
@@ -0,0 +1,28 @@
+
+-- Creating a new table Product_PriceComponent_Mapping
+PRINT 'Creating a new table Product_PriceComponent_Mapping'
+IF NOT EXISTS(SELECT 1 FROM SYS.OBJECTS WHERE name='Product_PriceComponent_Mapping' and type='U')
+BEGIN
+		CREATE TABLE [dbo].[Product_PriceComponent_Mapping](
+			[Id] [int] IDENTITY(1,1) NOT NULL,
+			[ProductId] [int] NOT NULL,
+			[PricecomponentId] [int] NOT NULL,
+			[Price] [decimal](18, 2) NOT NULL,
+			[Percentage] [decimal](18, 2) NOT NULL,
+			[Itemrate] [numeric](18, 2) NOT NULL,
+		 CONSTRAINT [PK_Product_PriceComponent_Mapping] PRIMARY KEY CLUSTERED 
+		(
+			[Id] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+		) ON [PRIMARY]
+		ALTER TABLE [dbo].[Product_PriceComponent_Mapping]  WITH CHECK ADD  CONSTRAINT [FK_Product_PriceComponent_Mapping_PriceComponents] FOREIGN KEY([PricecomponentId])
+		REFERENCES [dbo].[PriceComponents] ([PriceComponentId])
+		ALTER TABLE [dbo].[Product_PriceComponent_Mapping] CHECK CONSTRAINT [FK_Product_PriceComponent_Mapping_PriceComponents]
+		ALTER TABLE [dbo].[Product_PriceComponent_Mapping]  WITH CHECK ADD  CONSTRAINT [FK_Product_PriceComponent_Mapping_Product] FOREIGN KEY([ProductId])
+		REFERENCES [dbo].[Product] ([Id])
+		ALTER TABLE [dbo].[Product_PriceComponent_Mapping] CHECK CONSTRAINT [FK_Product_PriceComponent_Mapping_Product]
+		PRINT 'Created a new table Product_PriceComponent_Mapping'	
+END
+ELSE
+	PRINT 'Table Product_PriceComponent_Mapping Exists'
+GO
---
+++ Dev/DatabaseScripts/Tables/CreateTable/dbo_Product_ProductComponent_Mapping.sql
@@ -0,0 +1,25 @@
+
+-- Creating a new table Product_ProductComponent_Mapping
+PRINT 'Creating a new table Product_ProductComponent_Mapping'
+IF NOT EXISTS(SELECT 1 FROM SYS.OBJECTS WHERE name='Product_ProductComponent_Mapping' and type='U')
+BEGIN
+		CREATE TABLE [dbo].[Product_ProductComponent_Mapping](
+			[Id] [int] IDENTITY(1,1) NOT NULL,
+			[ProductId] [int] NOT NULL,
+			[ComponentId] [int] NOT NULL,
+		 CONSTRAINT [PK_Product_ProductComponent_Mapping] PRIMARY KEY CLUSTERED 
+		(
+			[Id] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+		) ON [PRIMARY]
+		ALTER TABLE [dbo].[Product_ProductComponent_Mapping]  WITH CHECK ADD  CONSTRAINT [FK_Product_ProductComponent_Mapping_Product] FOREIGN KEY([ProductId])
+		REFERENCES [dbo].[Product] ([Id])
+		ALTER TABLE [dbo].[Product_ProductComponent_Mapping] CHECK CONSTRAINT [FK_Product_ProductComponent_Mapping_Product]
+		ALTER TABLE [dbo].[Product_ProductComponent_Mapping]  WITH CHECK ADD  CONSTRAINT [FK_Product_ProductComponent_Mapping_ProductComponents] FOREIGN KEY([ComponentId])
+		REFERENCES [dbo].[ProductComponents] ([ComponentId])
+		ALTER TABLE [dbo].[Product_ProductComponent_Mapping] CHECK CONSTRAINT [FK_Product_ProductComponent_Mapping_ProductComponents]
+		PRINT 'Created a new table Product_ProductComponent_Mapping'	
+END
+ELSE
+	PRINT 'Table Product_ProductComponent_Mapping Exists'
+GO
