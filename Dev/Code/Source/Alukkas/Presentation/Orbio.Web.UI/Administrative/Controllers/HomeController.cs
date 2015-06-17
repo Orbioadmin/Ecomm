f3166e57e2d768886ec41bce570af26a4578f23f
sankardevts <sankar@orbiosolutions.com>
2015-06-17 10:24:18 -0700

    Admin project

---
+++ Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Administrative/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nop.Core.Infrastructure;
+using System.Web.Mvc;
+
+namespace Orbio.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TopMenu()
+        {
+            return PartialView();
+        }
+    }
+}
