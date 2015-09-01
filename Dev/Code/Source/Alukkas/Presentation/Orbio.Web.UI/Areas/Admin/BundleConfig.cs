99f613773eba074e3fd4342b1aacb9bc2c235cba
sankardevts <sankar@orbiosolutions.com>
2015-09-01 16:05:05 +0530

    change style in admin edit order details

--- Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Areas/Admin/BundleConfig.cs
+++ Dev/Code/Source/Alukkas/Presentation/Orbio.Web.UI/Areas/Admin/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace Orbio.Web.UI.Areas.Admin
                         "~/Areas/Admin/Scripts/jquery-{version}.js", "~/Areas/Admin/Scripts/bootstrap.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryuiadmin").Include("~/Areas/Admin/Scripts/jquery.js",
-                        "~/Areas/Admin/Scripts/less.js", "~/Areas/Admin/Scripts/ie-emulation-modes-warning.js", "~/Areas/Admin/Scripts/ie10-viewport-bug-workaround.js", "~/Areas/Admin/Scripts/highcharts.js", "~/Areas/Admin/Scripts/kendo.all.min.js"));
+                        "~/Areas/Admin/Scripts/less.js", "~/Areas/Admin/Scripts/ie-emulation-modes-warning.js", "~/Areas/Admin/Scripts/ie10-viewport-bug-workaround.js", "~/Areas/Admin/Scripts/kendo.all.min.js")
+                        );
 
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -25,6 +26,7 @@ namespace Orbio.Web.UI.Areas.Admin
 
             bundles.Add(new StyleBundle("~/Content/cssadmin").Include("~/Areas/Admin/Content/bootstrap.min.css", "~/Areas/Admin/Content/style.css", "~/Areas/Admin/Content/*.css",
                 "~/Areas/Admin/Content/kendo.common.min.css", "~/Areas/Admin/Content/kendo.default.min.css", "~/Areas/Admin/Content/kendo.dataviz.min.css", "~/Areas/Admin/Content/kendo.dataviz.default.min.css"));
+            new CssRewriteUrlTransform();
         }
     }
 }
